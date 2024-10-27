5792c5cd816cb7f145fe3cc7311a7521ee8503a1
cel004 <ngcelinaa@gmail.com>
2024-10-27 02:36:01 -0400

    Merge rightNav with contacts

--- portfolio-website/src/Header.jsx
+++ portfolio-website/src/Header.jsx
@@ -14,25 +14,26 @@ function Header(){
                         <li><a href = "contact">contact</a></li>
                     </ul>
                     </div>
-                    <ul className='rightNav'>
-                        <li><a href="linkedin"></a><LineMdLinkedin /></li>
-                        <li><a href="github"></a><LineMdGithub /></li>
-                    </ul>
                     
             </nav>
 
             <div className="circle-container">
                 <div className="circle" />
             </div>
-            
+
             <div className = "title">
                 <h1>Hi! I'm Celina!</h1>
                 <p>self-taught software developer</p>
             </div>
             
             <div className = "section-email">
-                <a href="mailto:name@gmail.com"><LineMdEmail /></a>
+                
                 <div id = "email">name@gmail.com</div>
+                <ul>
+                    <li><a href="mailto:name@gmail.com"></a><LineMdEmail /></li>
+                    <li><a href="linkedin"></a><LineMdLinkedin /></li>
+                    <li><a href="github"></a><LineMdGithub /></li>
+                </ul>
             </div>
                 
             
