9bd1c5df08a9df18c0f4e7c21deb6aefcca40f98
cel004 <ngcelinaa@gmail.com>
2024-10-26 21:50:58 -0400

    Import header and basic navigation for portfolio

---
+++ portfolio-website/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li><a href = "home">home</a></li>
+                    <li><a href = "projects">projects</a></li>
+                    <li><a href = "contact">contact</a></li>
+                </ul>
+            </nav>
+            <h1>hi! i'm celina!</h1>
+            <p>self-taught software developer</p>
+            <p></p>
+        </header>
+    )
+}
+
+export default Header
---
+++ portfolio-website/src/Website.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx'
+
+
+function Website() {
+
+  return(
+    <>
+    <Header></Header>
+
+    </>
+    
+  );
+
+}
+
+export default Website
