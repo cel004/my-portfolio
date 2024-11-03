d1c777a30eae16cbfc29a99a3cfbc259f6990fcf
cel004 <ngcelinaa@gmail.com>
2024-11-02 22:27:14 -0400

    Replace moon icon

--- portfolio-website/src/NightMode/NightMode.jsx
+++ portfolio-website/src/NightMode/NightMode.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import styles from './NightMode.module.css'
 
+import { LineMdSunnyOutlineToMoonAltLoopTransition } from '../assets/iconLinkedin';
+import { LineMdSunRisingLoop } from '../assets/iconGithub';
+
 const NightMode = () => {
     const [isNightMode, setIsNightMode] = useState(false);
 
