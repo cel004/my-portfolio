de2e9feb445912dc560cb486fd174c313bd14183
cel004 <ngcelinaa@gmail.com>
2024-11-03 16:08:24 -0500

    Import react and improve duplication function

--- portfolio-website/src/Scroll/Scroll.jsx
+++ portfolio-website/src/Scroll/Scroll.jsx
@@ -1,23 +1,30 @@
+import React from 'react';
+import styles from './Scroll.module.css'
+
 import { FlowbiteCssSolid } from '../assets/Skill/iconCSS';
 import { FlowbiteHtmlSolid } from '../assets/Skill/iconHTML';
 import { MdiReact } from '../assets/Skill/iconReact';
 import { LineiconsJavascript } from '../assets/Skill/iconJS';
 
-const duplicateIcons = [...icons, ...icons];
 
 function Scroll(){
     const icons = [
                 <li key = "css"><FlowbiteCssSolid /></li>,
-                <li key = "html "><FlowbiteHtmlSolid /></li>,
-                <li key = "react "><MdiReact /></li>,
-                <li key = "js "><LineiconsJavascript /></li>
+                <li key = "html"><FlowbiteHtmlSolid /></li>,
+                <li key = "react"><MdiReact /></li>,
+                <li key = "js"><LineiconsJavascript /></li>
     ];
+    
+    const duplicateIcons = [
+        ...icons.map((icon, index) => React.cloneElement(icon, { key: `icon-${index}` })),
+        ...icons.map((icon, index) => React.cloneElement(icon, { key: `icon-dup-${index}` })),
+    ];
+
     return (
-        <div className = "scroller">
-            <ul className = "scrollList">
+        <div className = {styles.scroller}>
+            <ul className = {styles.scrollList}>
                 {duplicateIcons}
             </ul>
-
         </div>
     );
 }
