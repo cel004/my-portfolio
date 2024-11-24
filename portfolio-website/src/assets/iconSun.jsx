4be1b06dea47c0f0d06369c52c07d0182b7e3d3c
cel004 <ngcelinaa@gmail.com>
2024-11-23 19:39:34 -0500

    Fix: font hierarchy

--- portfolio-website/src/assets/iconSun.jsx
+++ portfolio-website/src/assets/iconSun.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function LineMdSunRisingLoop(props) {
-	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" {...props}>
         <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
             <circle cx={12} cy={32} r={6}><animate fill="freeze" attributeName="cy" dur="0.6s" values="32;12">
                 </animate></circle>
