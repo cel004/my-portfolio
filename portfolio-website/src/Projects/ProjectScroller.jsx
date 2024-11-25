18189db9e7588c59dd4c212913acac769f4e81c8
cel004 <ngcelinaa@gmail.com>
2024-11-25 14:43:45 -0500

    Update project card

--- portfolio-website/src/Projects/ProjectScroller.jsx
+++ portfolio-website/src/Projects/ProjectScroller.jsx
@@ -2,6 +2,16 @@ import React, { useEffect, useRef } from 'react';
 import styles from './ProjectScroller.module.css';
 import Projects from './Projects.jsx';
 
+import { FlowbiteCssSolid } from '../assets/Skill/iconCSS';
+import { FlowbiteHtmlSolid } from '../assets/Skill/iconHTML';
+import { MdiReact } from '../assets/Skill/iconReact';
+import { LineiconsJavascript } from '../assets/Skill/iconJS';
+import { MingcuteLinuxFill } from '../assets/Skill/iconLinux';
+import { MdiGit } from '../assets/Skill/iconGit';
+import { DeviconPlainCsharp } from '../assets/Skill/iconCSharp';
+import { TablerBrandMysql } from '../assets/Skill/iconMySQL';
+import { Chartjs } from '../assets/Skill/iconChartJS';
+
 function ProjectScroller() {
     const galleryRef = useRef(null);
 
@@ -23,7 +33,12 @@ function ProjectScroller() {
 	<>
 	<h2>Projects</h2>
     <div className={styles.projectCard}>
-        <h3>Fruitful</h3>
+        <div className={styles.projectTitle}>
+            <h3>Fruitful</h3>
+            <div className={styles.projectSkills}>
+                <FlowbiteCssSolid/><FlowbiteHtmlSolid/><MdiReact/><TablerBrandMysql/><Chartjs/>
+            </div>
+        </div>
             <p className={styles.projectDescription}>
             A full-stack budgeting app that allows users 
             to track and manage finances with secure data storage.
